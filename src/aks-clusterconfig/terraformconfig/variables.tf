8d40937f1d7b60ae4aadd5ed77e06c6b2222dd27
delicmomcilo <momcilodelic@gmail.com>
2020-11-04 02:10:32 +0100

    Configured kubernetes cluster with terraform - deployed containerized app

---
+++ src/aks-clusterconfig/terraformconfig/variables.tf
@@ -0,0 +1,30 @@
+variable "resourcename" {
+  default = "pomodoro-k8s-resources"
+}
+variable "clustername" {
+  default = "kubernetes-aks1"
+}
+variable "location" {
+  default = "North Europe"
+}
+variable "dnspreffix" {
+  default = "kubecluster"
+}
+variable "size" {
+  default = "Standard_DS1_v2"
+}
+variable "agentnode" {
+  default = "2"
+}
+variable "subscription_id" {
+  default = "0561045a-1336-4069-9c60-15d1d53507aa"
+}
+variable "client_id" {
+  default = "912e0421-0b4f-49c4-9e48-639d86baa655"
+}
+variable "client_secret" {
+  default = "aH-ODJC3H_uWHPn8iAuIMiG7YykEJxX-Hu"
+}
+variable "tenant_id" {
+  default = "b044581a-d4be-48b0-8371-275e3f02cbcc"
+}
